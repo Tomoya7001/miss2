eae5f48853362be10c43749946794afa7f5c32f4
Tomoya7001 <spmiwdar7001@gmail.com>
2022-04-14 10:29:40 +0000

    credential.ymlの再生成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "f450dbbc096c4b3aa713550b87e45c66.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
 end
